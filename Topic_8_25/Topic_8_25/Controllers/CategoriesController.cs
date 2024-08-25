85d9b173127d9f1f0f3f72cc91aa8b8b8a491713
Hadeel-Alzoubi <hadeelzoubi12@gmail.com>
2024-08-25 23:53:51 +0300

    Core_Front

---
+++ Topic_8_25/Topic_8_25/Controllers/CategoriesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Topic_8_25.Models;
+
+namespace Topic_8_25.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        public MyDbContext db;
+
+        public CategoriesController(MyDbContext _db)
+        {
+            db = _db;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var category = db.Categories.ToList();
+            return Ok(category);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var category = db.Categories.Where(c => c.CategoryId == id);
+            return Ok(category);
+        }
+
+    }
+}
---
+++ Topic_8_25/Topic_8_25/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Topic_8_25.Models;
+
+namespace Topic_8_25.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public MyDbContext db;
+
+        public ProductController(MyDbContext _db)
+        {
+            db = _db;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var product = db.Products.ToList();
+            return Ok(product);
+        }
+
+        [HttpGet("getProduct/{product}")]
+        public IActionResult GetBy(int product)
+        {
+            var products = db.Products.Where(p => p.ProductId == product);
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var product = db.Products.Where(p => p.Category.CategoryId == id).Select(p => new
+            {
+                p.ProductId,
+                p.ProductName,
+                p.ProductImage,
+                p.Price,
+                Category = new
+                {
+                    p.Category.CategoryId,
+                    p.Category.CategoryImage,
+                    p.Category.CategoryName
+                }
+            }).ToList();
+            return Ok(product);
+        }
+    }
+}
